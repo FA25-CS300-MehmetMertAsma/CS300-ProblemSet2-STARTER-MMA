889201fbf520ac832effc376ac0e15c42357546e
Cowieian <cowieian@myocu.oak.edu>
2025-11-19 14:10:29 -0600

    Update books.js

--- routes/books.js
+++ routes/books.js
@@ -25,14 +25,14 @@ router.get('/', async (req, res) => {
 // GET /api/books/author/:lastName — get books by author last name
 router.get('/author/:lastName', async (req, res) => {
   try {
-    const { lastName } = req.params.lastName;
+    const { lastName } = req.params;
 
     const books = await Book.findAll({
       include: {
         model: Author,
         as: 'author',
         where: {
-          name: { [Op.Like]: `%${lastName}` } // case-insensitive match for last name
+          name: { [Op.like]: `%${lastName}` } // case-insensitive match for last name
         },
       },
     });
